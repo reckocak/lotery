ca76ce593bfdc7513c96aed635bbeaf100956ce6
baileyrec <kocakrecep68@gmail.com>
2022-07-06 16:47:00 +0200

    change

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const numberInput = document.getElementById("input");
 const Btn = document.getElementById("btn");
 
-
-
-
 let lotteryArray = [];
 let siebenNum = false;
 
 Btn.onclick = () => {
   let number = numberInput.value;
-    lotteryArray = [];
-    LuckyNumbers(number);
- 
-  
+  lotteryArray = [];
+  LuckyNumbers(number);
 };
 
 const LuckyNumbers = (i) => {
@@ -22,32 +17,27 @@ const LuckyNumbers = (i) => {
   for (let index = 0; index < i; index++) {
     for (let index = 0; index < 8; index++) {
       luckyNumber = Math.ceil(Math.random() * 90);
+
       lotteryArray[index] = luckyNumber;
     }
     for (let index = 0; index < 6; index++) {
-      
       if (lotteryArray[6] == lotteryArray[index]) {
-        // 7. sayi mutlaka farkli olsun
         siebenNum = true;
       }
       for (let index = 0; index < 5; index++) {
-        if (lotteryArray[index] > lotteryArray[index + 1]) {
-          let bigNum = lotteryArray[index];
-          lotteryArray[index] = lotteryArray[index + 1];
-          lotteryArray[index + 1] = bigNum;
-        }
+        lotteryArray.sort(function(a,b){return(a-b)});
         if (siebenNum) {
           lotteryArray[6] = Math.round(Math.random() * 90);
           siebenNum = true;
         }
       }
     }
-    screen(lotteryArray);
+    display(lotteryArray);
     console.log(lotteryArray);
   }
 };
 
-const screen = (array) => {
+const display = (array) => {
   document.querySelector(".root").innerHTML += `
       
         <p>${array[0]} - ${array[1]} - ${array[2]} - ${array[3]} - ${array[4]} - ${array[5]} | ${array[6]} | ${array[7]}</p> <hr>
