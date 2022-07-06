b102afa5a7c2528458ce5dcec7326d4286dccdca
baileyrec <kocakrecep68@gmail.com>
2022-07-06 22:06:41 +0200

    4

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const numberInput = document.getElementById("input");
 const Btn = document.getElementById("btn");
+const sonuc = document.querySelector(".root");
 
-let lotteryArray = [];
+let sayilar = [];
 let siebenNum = false;
-
-Btn.onclick = () => {
-  let number = numberInput.value;
-  lotteryArray = [];
-  LuckyNumbers(number);
-};
 
 const LuckyNumbers = (i) => {
   document.querySelector(".root").textContent = "";
@@ -16,24 +11,30 @@ const LuckyNumbers = (i) => {
 
   for (let index = 0; index < i; index++) {
     for (let index = 0; index < 8; index++) {
-      luckyNumber = Math.ceil(Math.random() * 90);
-
-      lotteryArray[index] = luckyNumber;
+      luckyNumber = Math.ceil(Math.random() * 89) + 1;
+      if (!sayilar.includes(luckyNumber)) {
+        sayilar[index] = luckyNumber;
+      }
     }
     for (let index = 0; index < 6; index++) {
-      if (lotteryArray[6] == lotteryArray[index]) {
-        siebenNum = true;
+      for (let index = 0; index < 6; index++) {
+        sayilar.sort(function (a, b) {
+          return a - b;
+        });
       }
-      for (let index = 0; index < 5; index++) {
-        lotteryArray.sort(function(a,b){return(a-b)});
+      for (let index = 0; index < 6; index++) {
+        sayilar.sort(function (a, b) {
+          return a - b;
+        });
+
         if (siebenNum) {
-          lotteryArray[6] = Math.round(Math.random() * 90);
+          sayilar[6] = Math.round(Math.random() * 90);
           siebenNum = true;
         }
       }
     }
-    display(lotteryArray);
-    console.log(lotteryArray);
+    display(sayilar);
+    console.log(sayilar);
   }
 };
 
@@ -42,4 +43,9 @@ const display = (array) => {
       
         <p>${array[0]} - ${array[1]} - ${array[2]} - ${array[3]} - ${array[4]} - ${array[5]} | ${array[6]} | ${array[7]}</p> <hr>
     `;
+};
+Btn.onclick = () => {
+  let number = numberInput.value;
+  sayilar = [];
+  LuckyNumbers(number);
 };
